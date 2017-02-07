4a9eb008f80581b8979ffd9882359e1bf3003f1d
Ricardo Pereira <m@ricardopereira.eu>
2017-02-07 15:08:50 +0000

    Reports: loading data

--- routes/views/reports.js
+++ routes/views/reports.js
@@ -1,14 +1,26 @@
 var keystone = require('keystone');
-var async = require('async');
 
 exports = module.exports = function(req, res) {
 
 	var view = new keystone.View(req, res);
 	var locals = res.locals;
 
-	// locals.section is used to set the currently selected
-	// item in the header navigation.
-	locals.section = 'home';
+	locals.section = 'reports';
+	locals.data = {
+		countries: []
+	};
+
+	// Load data
+	view.on('init', function(next) {
+
+		var q = keystone.list('Country').model.find();
+
+		q.exec(function(err, results) {
+			locals.data.countries = results;
+			next(err);
+		});
+
+	});
 
 	// Render the view
 	view.render('reports');
